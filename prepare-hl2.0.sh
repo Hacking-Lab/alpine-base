c650eeea24f1ee990abc7bcaede20bf175a79ca5
Ivan Bütler <ivan.buetler@compass-security.com>
2020-01-01 17:07:38 +0100

    update with prepare hl 2.0 script

---
+++ prepare-hl2.0.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "What is the resource UUID? This is required! Please enter the UUID, followed by [ENTER]:"
+read id
+
+
+if [ -d ./deploy ]; then
+    echo "deploy folder available - will delete the folder"
+    rm -rf ./deploy
+    sleep 2
+else
+    echo "deploy not available"
+fi
+
+mkdir ./deploy
+cp -rf ./root/ ./deploy/
+cp ./Dockerfile ./deploy
+cp ./UUID.env ./deploy/$id.env
+cp ./UUID.gn ./deploy/$id.gn
+cd ./deploy
+tar -cvzf dockerfiles.tar.gz *
+rm Dockerfile
+rm -rf root
+rm $id.env
+rm $id.gn
+cd ..
+ls -al ./deploy 
